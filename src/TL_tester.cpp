8906062bcc4392636211df0fe15946148db86e38
AgostoSound <agostosound@gmail.com>
2025-05-31 16:29:43 +0200

    showroom of knobs

--- src/TL_tester.cpp
+++ src/TL_tester.cpp
@@ -15,6 +15,14 @@ struct TL_tester : Module {
 		KNOB10_PARAM,
 		KNOB11_PARAM,
 		KNOB12_PARAM,
+		KNOB13_PARAM,
+		KNOB14_PARAM,
+		KNOB15_PARAM,
+		KNOB16_PARAM,
+		KNOB17_PARAM,
+		KNOB18_PARAM,
+		KNOB19_PARAM,
+		KNOB20_PARAM,
 		PARAMS_LEN
 	};
 	enum InputId {
@@ -25,10 +33,6 @@ struct TL_tester : Module {
 		INPUTS_LEN
 	};
 	enum OutputId {
-		OUT1_OUTPUT,
-		OUT2_OUTPUT,
-		OUT3_OUTPUT,
-		OUT4_OUTPUT,
 		OUTPUTS_LEN
 	};
 	enum LightId {
@@ -60,17 +64,19 @@ struct TL_tester : Module {
 		configParam(KNOB9_PARAM, 0.f, 1.f, 0.f, "");
 		configParam(KNOB10_PARAM, 0.f, 1.f, 0.f, "");
 		configParam(KNOB11_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(KNOB12_PARAM, 0.f, 1.f, 0.f, "");
+		configParam(KNOB13_PARAM, 0.f, 1.f, 0.f, "");
+		configParam(KNOB14_PARAM, 0.f, 1.f, 0.f, "");
+		configParam(KNOB15_PARAM, 0.f, 1.f, 0.f, "");
+		configParam(KNOB16_PARAM, 0.f, 1.f, 0.f, "");
+		configParam(KNOB17_PARAM, 0.f, 1.f, 0.f, "");
+		configParam(KNOB18_PARAM, 0.f, 1.f, 0.f, "");
+		configParam(KNOB19_PARAM, 0.f, 1.f, 0.f, "");
+		configParam(KNOB20_PARAM, 0.f, 1.f, 0.f, "");
 
 		configInput(PARAM1_INPUT, "");
 		configInput(PARAM4_INPUT, "");
 		configInput(PARAM3_INPUT, "");
 		configInput(PARAM2_INPUT, "");
-
-		configOutput(OUT2_OUTPUT, "");
-		configOutput(OUT1_OUTPUT, "");
-		configOutput(OUT4_OUTPUT, "");
-		configOutput(OUT3_OUTPUT, "");
 	}
 
 	void process(const ProcessArgs& args) override {
@@ -100,11 +106,14 @@ struct TL_testerWidget : ModuleWidget {
 		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
+
 		// INPUTS
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(8.991, 8.157)), module, TL_tester::PARAM1_INPUT));
 		addInput(createInputCentered<DarkPJ301MPort>(mm2px(Vec(22.353, 8.588)), module, TL_tester::PARAM2_INPUT));
 		addInput(createInputCentered<CL1362Port>(mm2px(Vec(35.515, 8.373)), module, TL_tester::PARAM3_INPUT));
 		addInput(createInputCentered<PJ3410Port>(mm2px(Vec(49.296, 8.114)), module, TL_tester::PARAM4_INPUT));
+
+
 
 		// KNOBS / SWITCHES (3 filas distintas)
 		addParam(createParamCentered<RoundSmallBlackKnob>(mm2px(Vec(8.5, 25.0)), module, TL_tester::KNOB1_PARAM));
@@ -119,30 +128,38 @@ struct TL_testerWidget : ModuleWidget {
 
 		addParam(createParamCentered<TL1105>(mm2px(Vec(8.5, 55.0)), module, TL_tester::KNOB9_PARAM));
 		addParam(createParamCentered<Trimpot>(mm2px(Vec(22.5, 55.0)), module, TL_tester::KNOB10_PARAM));
-		addParam(createParamCentered<BefacoTinyKnob>(mm2px(Vec(36.5, 55.0)), module, TL_tester::KNOB11_PARAM));
-		addParam(createParamCentered<BefacoPush>(mm2px(Vec(50.5, 55.0)), module, TL_tester::KNOB12_PARAM));
+		addParam(createParamCentered<Davies1900hBlackKnob>(mm2px(Vec(36.5, 55.0)), module, TL_tester::KNOB11_PARAM));
+		addParam(createParamCentered<NKK>(mm2px(Vec(50.5, 55.0)), module, TL_tester::KNOB12_PARAM));
+
+		addParam(createParamCentered<Davies1900hLargeWhiteKnob>(mm2px(Vec(8.5, 70.0)), module, TL_tester::KNOB13_PARAM));
+		addParam(createParamCentered<Davies1900hWhiteKnob>(mm2px(Vec(22.5, 70.0)), module, TL_tester::KNOB14_PARAM));
+		addParam(createParamCentered<PB61303>(mm2px(Vec(36.5, 70.0)), module, TL_tester::KNOB15_PARAM));
+		addParam(createParamCentered<CKSSThreeHorizontal>(mm2px(Vec(50.5, 70.0)), module, TL_tester::KNOB16_PARAM));
+
+		addParam(createParamCentered<BefacoTinyKnob>(mm2px(Vec(8.5, 85.0)), module, TL_tester::KNOB17_PARAM));
+		addParam(createParamCentered<Rogan5PSGray>(mm2px(Vec(22.5, 85.0)), module, TL_tester::KNOB18_PARAM));
+		addParam(createParamCentered<Rogan1PBlue>(mm2px(Vec(36.5, 85.0)), module, TL_tester::KNOB19_PARAM));
+		addParam(createParamCentered<BefacoBigKnob>(mm2px(Vec(50.5, 85.0)), module, TL_tester::KNOB20_PARAM));
+
+
+
 
 		// LEDS (3 filas distintos tipos)
-		addChild(createLightCentered<TinyLight<RedLight>>(mm2px(Vec(8.5, 70.0)), module, TL_tester::LED1_LIGHT));
-		addChild(createLightCentered<SmallLight<GreenLight>>(mm2px(Vec(22.5, 70.0)), module, TL_tester::LED2_LIGHT));
-		addChild(createLightCentered<MediumLight<BlueLight>>(mm2px(Vec(36.5, 70.0)), module, TL_tester::LED3_LIGHT));
-		addChild(createLightCentered<LargeLight<WhiteLight>>(mm2px(Vec(50.5, 70.0)), module, TL_tester::LED4_LIGHT));
+		addChild(createLightCentered<TinyLight<RedLight>>(mm2px(Vec(8.5, 100.0)), module, TL_tester::LED1_LIGHT));
+		addChild(createLightCentered<SmallLight<GreenLight>>(mm2px(Vec(22.5, 100.0)), module, TL_tester::LED2_LIGHT));
+		addChild(createLightCentered<MediumLight<BlueLight>>(mm2px(Vec(36.5, 100.0)), module, TL_tester::LED3_LIGHT));
+		addChild(createLightCentered<LargeLight<WhiteLight>>(mm2px(Vec(50.5, 100.0)), module, TL_tester::LED4_LIGHT));
 
-		addChild(createLightCentered<SmallLight<BlueLight>>(mm2px(Vec(8.5, 80.0)), module, TL_tester::LED5_LIGHT));
-		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(22.5, 80.0)), module, TL_tester::LED6_LIGHT));
-		addChild(createLightCentered<LargeLight<GreenLight>>(mm2px(Vec(36.5, 80.0)), module, TL_tester::LED7_LIGHT));
-		addChild(createLightCentered<TinyLight<WhiteLight>>(mm2px(Vec(50.5, 80.0)), module, TL_tester::LED8_LIGHT));
+		addChild(createLightCentered<SmallLight<BlueLight>>(mm2px(Vec(8.5, 110.0)), module, TL_tester::LED5_LIGHT));
+		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(22.5, 110.0)), module, TL_tester::LED6_LIGHT));
+		addChild(createLightCentered<LargeLight<GreenLight>>(mm2px(Vec(36.5, 110.0)), module, TL_tester::LED7_LIGHT));
+		addChild(createLightCentered<TinyLight<WhiteLight>>(mm2px(Vec(50.5, 110.0)), module, TL_tester::LED8_LIGHT));
 
-		addChild(createLightCentered<LargeLight<BlueLight>>(mm2px(Vec(8.5, 90.0)), module, TL_tester::LED9_LIGHT));
-		addChild(createLightCentered<MediumLight<WhiteLight>>(mm2px(Vec(22.5, 90.0)), module, TL_tester::LED10_LIGHT));
-		addChild(createLightCentered<SmallLight<RedLight>>(mm2px(Vec(36.5, 90.0)), module, TL_tester::LED11_LIGHT));
-		addChild(createLightCentered<TinyLight<GreenLight>>(mm2px(Vec(50.5, 90.0)), module, TL_tester::LED12_LIGHT));
+		addChild(createLightCentered<LargeLight<BlueLight>>(mm2px(Vec(8.5, 120.0)), module, TL_tester::LED9_LIGHT));
+		addChild(createLightCentered<MediumLight<WhiteLight>>(mm2px(Vec(22.5, 120.0)), module, TL_tester::LED10_LIGHT));
+		addChild(createLightCentered<SmallLight<RedLight>>(mm2px(Vec(36.5, 120.0)), module, TL_tester::LED11_LIGHT));
+		addChild(createLightCentered<TinyLight<GreenLight>>(mm2px(Vec(50.5, 120.0)), module, TL_tester::LED12_LIGHT));
 
-		// OUTPUTS (4 variantes de port)
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(8.5, 105.0)), module, TL_tester::OUT1_OUTPUT));
-		addOutput(createOutputCentered<DarkPJ301MPort>(mm2px(Vec(22.5, 105.0)), module, TL_tester::OUT2_OUTPUT));
-		addOutput(createOutputCentered<CL1362Port>(mm2px(Vec(36.5, 105.0)), module, TL_tester::OUT3_OUTPUT));
-		addOutput(createOutputCentered<PJ3410Port>(mm2px(Vec(50.5, 105.0)), module, TL_tester::OUT4_OUTPUT));
 	}
 };
 
