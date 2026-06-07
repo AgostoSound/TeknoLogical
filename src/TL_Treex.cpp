9ef05df466c6d29e7ccc8b3bae57a937b3583d0b
AgostoSound <agostosound@gmail.com>
2026-06-07 09:52:06 +0200

    Normaliza nombre de variables + ajusta centrado de ouput jacks.

--- src/TL_Treex.cpp
+++ src/TL_Treex.cpp
@@ -9,37 +9,37 @@
 
 struct TL_Treex : Module {
 	enum ParamId {
+		CUT_1_PARAM,
+		CUT_2_PARAM,
 		CUT_3_PARAM,
-		CUT_4_PARAM,
-		CUT_5_PARAM,
+		PAN_1_PARAM,
+		PAN_2_PARAM,
 		PAN_3_PARAM,
-		PAN_4_PARAM,
-		PAN_5_PARAM,
+		VOL_1_PARAM,
+		VOL_2_PARAM,
 		VOL_3_PARAM,
-		VOL_4_PARAM,
-		VOL_5_PARAM,
+		MUTE_1_PARAM,
+		MUTE_2_PARAM,
 		MUTE_3_PARAM,
-		MUTE_4_PARAM,
-		MUTE_5_PARAM,
+		SOLO_1_PARAM,
+		SOLO_2_PARAM,
 		SOLO_3_PARAM,
-		SOLO_4_PARAM,
-		SOLO_5_PARAM,
 		PARAMS_LEN
 	};
 
 	enum InputId {
+		L_IN_1_INPUT,
+		L_IN_2_INPUT,
 		L_IN_3_INPUT,
-		L_IN_4_INPUT,
-		L_IN_5_INPUT,
+		R_IN_1_INPUT,
+		R_IN_2_INPUT,
 		R_IN_3_INPUT,
-		R_IN_4_INPUT,
-		R_IN_5_INPUT,
+		VOL_IN_1_INPUT,
+		VOL_IN_2_INPUT,
 		VOL_IN_3_INPUT,
-		VOL_IN_4_INPUT,
-		VOL_IN_5_INPUT,
+		PAN_IN_1_INPUT,
+		PAN_IN_2_INPUT,
 		PAN_IN_3_INPUT,
-		PAN_IN_4_INPUT,
-		PAN_IN_5_INPUT,
 		INPUTS_LEN
 	};
 
@@ -50,9 +50,9 @@ struct TL_Treex : Module {
 	};
 
 	enum LightId {
+		LED_1_LIGHT,
+		LED_2_LIGHT,
 		LED_3_LIGHT,
-		LED_4_LIGHT,
-		LED_5_LIGHT,
 		L_VU_1_LIGHT,
 		L_VU_2_LIGHT,
 		L_VU_3_LIGHT,
@@ -63,12 +63,12 @@ struct TL_Treex : Module {
 		R_VU_3_LIGHT,
 		R_VU_4_LIGHT,
 		R_VU_5_LIGHT,
+		MUTE_1_LED,
+		MUTE_2_LED,
 		MUTE_3_LED,
-		MUTE_4_LED,
-		MUTE_5_LED,
+		SOLO_1_LED,
+		SOLO_2_LED,
 		SOLO_3_LED,
-		SOLO_4_LED,
-		SOLO_5_LED,
 		LIGHTS_LEN
 	};
 
@@ -94,16 +94,16 @@ struct TL_Treex : Module {
 		static const std::vector<std::string> onoffLabels = {"Off", "On"};
 
 		for (int c = 0; c < CH; ++c) {
-			configSwitch(CUT_3_PARAM + c, 0.f, 1.f, 0.f, "Cutoff", onoffLabels);
-			configParam(PAN_3_PARAM + c, -1.f, 1.f, 0.f, "Pan");
-			configParam(VOL_3_PARAM + c, 0.f, 10.f, 0.f, "Vol");
-			configSwitch(MUTE_3_PARAM + c, 0.f, 1.f, 0.f, "Mute", onoffLabels);
-			configSwitch(SOLO_3_PARAM + c, 0.f, 1.f, 0.f, "Solo", onoffLabels);
+			configSwitch(CUT_1_PARAM + c, 0.f, 1.f, 0.f, "Cutoff", onoffLabels);
+			configParam(PAN_1_PARAM + c, -1.f, 1.f, 0.f, "Pan");
+			configParam(VOL_1_PARAM + c, 0.f, 10.f, 0.f, "Vol");
+			configSwitch(MUTE_1_PARAM + c, 0.f, 1.f, 0.f, "Mute", onoffLabels);
+			configSwitch(SOLO_1_PARAM + c, 0.f, 1.f, 0.f, "Solo", onoffLabels);
 
-			configInput(L_IN_3_INPUT + c, "L audio");
-			configInput(R_IN_3_INPUT + c, "R audio");
-			configInput(VOL_IN_3_INPUT + c, "Vol CV");
-			configInput(PAN_IN_3_INPUT + c, "Pan CV");
+			configInput(L_IN_1_INPUT + c, "L audio");
+			configInput(R_IN_1_INPUT + c, "R audio");
+			configInput(VOL_IN_1_INPUT + c, "Vol CV");
+			configInput(PAN_IN_1_INPUT + c, "Pan CV");
 
 			hpL[c].setCutoff(cutHz, sampleRate);
 			hpR[c].setCutoff(cutHz, sampleRate);
@@ -169,28 +169,28 @@ struct TL_Treex : Module {
 
 		bool anySolo = false;
 		for (int c = 0; c < CH; ++c)
-			anySolo |= params[SOLO_3_PARAM + c].getValue() > 0.5f;
+			anySolo |= params[SOLO_1_PARAM + c].getValue() > 0.5f;
 
 		for (int c = 0; c < CH; ++c) {
-			bool cut = params[CUT_3_PARAM + c].getValue() > 0.5f;
-			bool mute = params[MUTE_3_PARAM + c].getValue() > 0.5f;
-			bool solo = params[SOLO_3_PARAM + c].getValue() > 0.5f;
+			bool cut = params[CUT_1_PARAM + c].getValue() > 0.5f;
+			bool mute = params[MUTE_1_PARAM + c].getValue() > 0.5f;
+			bool solo = params[SOLO_1_PARAM + c].getValue() > 0.5f;
 
-			lights[LED_3_LIGHT + c].setBrightness(cut ? 1.f : 0.f);
-			lights[MUTE_3_LED + c].setBrightness(mute ? 1.f : 0.f);
-			lights[SOLO_3_LED + c].setBrightness(solo ? 1.f : 0.f);
+			lights[LED_1_LIGHT + c].setBrightness(cut ? 1.f : 0.f);
+			lights[MUTE_1_LED + c].setBrightness(mute ? 1.f : 0.f);
+			lights[SOLO_1_LED + c].setBrightness(solo ? 1.f : 0.f);
 
 			if (mute || (anySolo && !solo))
 				continue;
 
-			bool lConnected = inputs[L_IN_3_INPUT + c].isConnected();
-			bool rConnected = inputs[R_IN_3_INPUT + c].isConnected();
+			bool lConnected = inputs[L_IN_1_INPUT + c].isConnected();
+			bool rConnected = inputs[R_IN_1_INPUT + c].isConnected();
 
 			if (!lConnected && !rConnected)
 				continue;
 
-			float inL = lConnected ? inputs[L_IN_3_INPUT + c].getVoltage() : 0.f;
-			float inR = rConnected ? inputs[R_IN_3_INPUT + c].getVoltage() : 0.f;
+			float inL = lConnected ? inputs[L_IN_1_INPUT + c].getVoltage() : 0.f;
+			float inR = rConnected ? inputs[R_IN_1_INPUT + c].getVoltage() : 0.f;
 
 			bool stereo = lConnected && rConnected;
 
@@ -206,19 +206,19 @@ struct TL_Treex : Module {
 			}
 
 			float vol = DSPUtils::resolveVolume01(
-				params[VOL_3_PARAM + c].getValue(),
-				inputs[VOL_IN_3_INPUT + c].isConnected(),
-				inputs[VOL_IN_3_INPUT + c].getVoltage()
+				params[VOL_1_PARAM + c].getValue(),
+				inputs[VOL_IN_1_INPUT + c].isConnected(),
+				inputs[VOL_IN_1_INPUT + c].getVoltage()
 			);
 
 			inL *= vol;
 			inR *= vol;
 
-			bool panCvConnected = inputs[PAN_IN_3_INPUT + c].isConnected();
+			bool panCvConnected = inputs[PAN_IN_1_INPUT + c].isConnected();
 
 			float pan = panCvConnected
-				? DSPUtils::resolvePanMinus1to1(0.f, true, inputs[PAN_IN_3_INPUT + c].getVoltage())
-				: params[PAN_3_PARAM + c].getValue();
+				? DSPUtils::resolvePanMinus1to1(0.f, true, inputs[PAN_IN_1_INPUT + c].getVoltage())
+				: params[PAN_1_PARAM + c].getValue();
 
 			updatePanCache(c, pan, panCvConnected);
 
@@ -306,54 +306,54 @@ struct TL_TreexWidget : ModuleWidget {
 			return mm2px(Vec(x - X_SHIFT, y + Y_SHIFT));
 		};
 
-		addParam(createParamCentered<NKK>(P(41.3, 48.233), module, TL_Treex::CUT_3_PARAM));
-		addParam(createParamCentered<NKK>(P(60.922, 46.174), module, TL_Treex::CUT_4_PARAM));
-		addParam(createParamCentered<NKK>(P(81.346, 48.171), module, TL_Treex::CUT_5_PARAM));
+		addParam(createParamCentered<NKK>(P(41.3, 48.233), module, TL_Treex::CUT_1_PARAM));
+		addParam(createParamCentered<NKK>(P(60.922, 46.174), module, TL_Treex::CUT_2_PARAM));
+		addParam(createParamCentered<NKK>(P(81.346, 48.171), module, TL_Treex::CUT_3_PARAM));
 
-		addParam(createParamCentered<Trimpot>(P(41.4, 62.026), module, TL_Treex::PAN_3_PARAM));
-		addParam(createParamCentered<Trimpot>(P(61.05, 59.835), module, TL_Treex::PAN_4_PARAM));
-		addParam(createParamCentered<Trimpot>(P(81.4, 61.898), module, TL_Treex::PAN_5_PARAM));
+		addParam(createParamCentered<Trimpot>(P(41.4, 62.026), module, TL_Treex::PAN_1_PARAM));
+		addParam(createParamCentered<Trimpot>(P(61.05, 59.835), module, TL_Treex::PAN_2_PARAM));
+		addParam(createParamCentered<Trimpot>(P(81.4, 61.898), module, TL_Treex::PAN_3_PARAM));
 
-		addParam(createParamCentered<Rogan1PWhite>(P(41.238, 75.329), module, TL_Treex::VOL_3_PARAM));
-		addParam(createParamCentered<Rogan1PWhite>(P(61.025, 73.204), module, TL_Treex::VOL_4_PARAM));
-		addParam(createParamCentered<Rogan1PWhite>(P(81.4, 75.267), module, TL_Treex::VOL_5_PARAM));
+		addParam(createParamCentered<Rogan1PWhite>(P(41.238, 75.329), module, TL_Treex::VOL_1_PARAM));
+		addParam(createParamCentered<Rogan1PWhite>(P(61.025, 73.204), module, TL_Treex::VOL_2_PARAM));
+		addParam(createParamCentered<Rogan1PWhite>(P(81.4, 75.267), module, TL_Treex::VOL_3_PARAM));
 
 		addParam(createLightParamCentered<VCVLightLatch<MediumSimpleLight<BlueLight>>>(P(36.764, 88.753), 
-		module, TL_Treex::MUTE_3_PARAM, TL_Treex::MUTE_3_LED));
+		module, TL_Treex::MUTE_1_PARAM, TL_Treex::MUTE_1_LED));
 		addParam(createLightParamCentered<VCVLightLatch<MediumSimpleLight<BlueLight>>>(P(57.31, 86.156), 
-		module, TL_Treex::MUTE_4_PARAM, TL_Treex::MUTE_4_LED));
+		module, TL_Treex::MUTE_2_PARAM, TL_Treex::MUTE_2_LED));
 		addParam(createLightParamCentered<VCVLightLatch<MediumSimpleLight<BlueLight>>>(P(77.6, 87.792), 
-		module, TL_Treex::MUTE_5_PARAM, TL_Treex::MUTE_5_LED));
+		module, TL_Treex::MUTE_3_PARAM, TL_Treex::MUTE_3_LED));
 
 		addParam(createLightParamCentered<VCVLightLatch<MediumSimpleLight<BlueLight>>>(P(45.29, 87.753), 
-		module, TL_Treex::SOLO_3_PARAM, TL_Treex::SOLO_3_LED));
+		module, TL_Treex::SOLO_1_PARAM, TL_Treex::SOLO_1_LED));
 		addParam(createLightParamCentered<VCVLightLatch<MediumSimpleLight<BlueLight>>>(P(65.6, 86.141), 
-		module, TL_Treex::SOLO_4_PARAM, TL_Treex::SOLO_4_LED));
+		module, TL_Treex::SOLO_2_PARAM, TL_Treex::SOLO_2_LED));
 		addParam(createLightParamCentered<VCVLightLatch<MediumSimpleLight<BlueLight>>>(P(86.05, 88.777), 
-		module, TL_Treex::SOLO_5_PARAM, TL_Treex::SOLO_5_LED));
+		module, TL_Treex::SOLO_3_PARAM, TL_Treex::SOLO_3_LED));
 
-		addInput(createInputCentered<PJ301MPort>(P(41.169, 6.017), module, TL_Treex::L_IN_3_INPUT));
-		addInput(createInputCentered<PJ301MPort>(P(60.956, 3.892), module, TL_Treex::L_IN_4_INPUT));
-		addInput(createInputCentered<PJ301MPort>(P(81.215, 5.955), module, TL_Treex::L_IN_5_INPUT));
+		addInput(createInputCentered<PJ301MPort>(P(41.169, 6.017), module, TL_Treex::L_IN_1_INPUT));
+		addInput(createInputCentered<PJ301MPort>(P(60.956, 3.892), module, TL_Treex::L_IN_2_INPUT));
+		addInput(createInputCentered<PJ301MPort>(P(81.215, 5.955), module, TL_Treex::L_IN_3_INPUT));
 
-		addInput(createInputCentered<PJ301MPort>(P(41.169, 15.564), module, TL_Treex::R_IN_3_INPUT));
-		addInput(createInputCentered<PJ301MPort>(P(60.956, 13.439), module, TL_Treex::R_IN_4_INPUT));
-		addInput(createInputCentered<PJ301MPort>(P(81.215, 15.502), module, TL_Treex::R_IN_5_INPUT));
+		addInput(createInputCentered<PJ301MPort>(P(41.169, 15.564), module, TL_Treex::R_IN_1_INPUT));
+		addInput(createInputCentered<PJ301MPort>(P(60.956, 13.439), module, TL_Treex::R_IN_2_INPUT));
+		addInput(createInputCentered<PJ301MPort>(P(81.215, 15.502), module, TL_Treex::R_IN_3_INPUT));
 
-		addInput(createInputCentered<PJ301MPort>(P(41.18, 27.162), module, TL_Treex::VOL_IN_3_INPUT));
-		addInput(createInputCentered<PJ301MPort>(P(60.967, 25.036), module, TL_Treex::VOL_IN_4_INPUT));
-		addInput(createInputCentered<PJ301MPort>(P(81.226, 27.1), module, TL_Treex::VOL_IN_5_INPUT));
+		addInput(createInputCentered<PJ301MPort>(P(41.18, 27.162), module, TL_Treex::VOL_IN_1_INPUT));
+		addInput(createInputCentered<PJ301MPort>(P(60.967, 25.036), module, TL_Treex::VOL_IN_2_INPUT));
+		addInput(createInputCentered<PJ301MPort>(P(81.226, 27.1), module, TL_Treex::VOL_IN_3_INPUT));
 
-		addInput(createInputCentered<PJ301MPort>(P(41.158, 36.687), module, TL_Treex::PAN_IN_3_INPUT));
-		addInput(createInputCentered<PJ301MPort>(P(60.945, 34.561), module, TL_Treex::PAN_IN_4_INPUT));
-		addInput(createInputCentered<PJ301MPort>(P(81.204, 36.625), module, TL_Treex::PAN_IN_5_INPUT));
+		addInput(createInputCentered<PJ301MPort>(P(41.158, 36.687), module, TL_Treex::PAN_IN_1_INPUT));
+		addInput(createInputCentered<PJ301MPort>(P(60.945, 34.561), module, TL_Treex::PAN_IN_2_INPUT));
+		addInput(createInputCentered<PJ301MPort>(P(81.204, 36.625), module, TL_Treex::PAN_IN_3_INPUT));
 
-		addOutput(createOutputCentered<DarkPJ301MPort>(P(56.5, 105.97), module, TL_Treex::OUT_L_OUTPUT));
-		addOutput(createOutputCentered<DarkPJ301MPort>(P(66.01, 106.1), module, TL_Treex::OUT_R_OUTPUT));
+		addOutput(createOutputCentered<DarkPJ301MPort>(P(56.7, 105.97), module, TL_Treex::OUT_L_OUTPUT));
+		addOutput(createOutputCentered<DarkPJ301MPort>(P(66.2, 106.1), module, TL_Treex::OUT_R_OUTPUT));
 
-		addChild(createLightCentered<TinyLight<WhiteLight>>(P(36.083, 45.158), module, TL_Treex::LED_3_LIGHT));
-		addChild(createLightCentered<TinyLight<WhiteLight>>(P(55.87, 43.033), module, TL_Treex::LED_4_LIGHT));
-		addChild(createLightCentered<TinyLight<WhiteLight>>(P(76.129, 45.096), module, TL_Treex::LED_5_LIGHT));
+		addChild(createLightCentered<TinyLight<WhiteLight>>(P(36.083, 45.158), module, TL_Treex::LED_1_LIGHT));
+		addChild(createLightCentered<TinyLight<WhiteLight>>(P(55.87, 43.033), module, TL_Treex::LED_2_LIGHT));
+		addChild(createLightCentered<TinyLight<WhiteLight>>(P(76.129, 45.096), module, TL_Treex::LED_3_LIGHT));
 
 		addChild(createLightCentered<TinyLight<WhiteLight>>(P(9.6, 115.7), module, TL_Treex::L_VU_1_LIGHT));
 		addChild(createLightCentered<TinyLight<WhiteLight>>(P(15.1, 115.7), module, TL_Treex::L_VU_2_LIGHT));
